c1fdd1e6ccd9e557c0a140fc95c58d959f2d3a60
j0sash1 <jorge.nunez@alumnos.ucn.cl>
2025-06-03 19:57:59 -0400

    feat(Src): Se crean las carpetas del proyecto con una estructura modular

---
+++ src/views/loginPage/LoginPage.tsx
@@ -0,0 +1,9 @@
+
+
+export const LoginPage = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+            <h1> Login Page cargada</h1>
+        </div>
+    )
+}
